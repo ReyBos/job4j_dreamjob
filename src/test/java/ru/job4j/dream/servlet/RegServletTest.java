1bef7261ba3ce2d6342cca4a69e609bf9ca4d5fc
reybos <andreybosiy@yandex.ru>
2021-02-16 17:16:23 +0300

    1. Написать тест на PostServlet. [#456072]

--- src/test/java/ru/job4j/dream/servlet/RegServletTest.java
+++ src/test/java/ru/job4j/dream/servlet/RegServletTest.java
@@ -10,14 +10,11 @@ import ru.job4j.dream.model.User;
 import ru.job4j.dream.store.MemStore;
 import ru.job4j.dream.store.PsqlStore;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.servlet.http.HttpSessionContext;
 import java.io.IOException;
-import java.util.Enumeration;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -34,95 +31,11 @@ public class RegServletTest {
         Mockito.when(PsqlStore.instOf()).thenReturn(store);
         HttpServletRequest req = mock(HttpServletRequest.class);
         HttpServletResponse resp = mock(HttpServletResponse.class);
+        HttpSession session = mock(HttpSession.class);
         when(req.getParameter("name")).thenReturn("Andrew");
         when(req.getParameter("email")).thenReturn("test");
         when(req.getParameter("password")).thenReturn("12345");
-        when(req.getSession()).thenReturn(new HttpSession() {
-            @Override
-            public long getCreationTime() {
-                return 0;
-            }
-
-            @Override
-            public String getId() {
-                return null;
-            }
-
-            @Override
-            public long getLastAccessedTime() {
-                return 0;
-            }
-
-            @Override
-            public ServletContext getServletContext() {
-                return null;
-            }
-
-            @Override
-            public void setMaxInactiveInterval(int i) {
-
-            }
-
-            @Override
-            public int getMaxInactiveInterval() {
-                return 0;
-            }
-
-            @Override
-            public HttpSessionContext getSessionContext() {
-                return null;
-            }
-
-            @Override
-            public Object getAttribute(String s) {
-                return null;
-            }
-
-            @Override
-            public Object getValue(String s) {
-                return null;
-            }
-
-            @Override
-            public Enumeration<String> getAttributeNames() {
-                return null;
-            }
-
-            @Override
-            public String[] getValueNames() {
-                return new String[0];
-            }
-
-            @Override
-            public void setAttribute(String s, Object o) {
-
-            }
-
-            @Override
-            public void putValue(String s, Object o) {
-
-            }
-
-            @Override
-            public void removeAttribute(String s) {
-
-            }
-
-            @Override
-            public void removeValue(String s) {
-
-            }
-
-            @Override
-            public void invalidate() {
-
-            }
-
-            @Override
-            public boolean isNew() {
-                return false;
-            }
-        });
+        when(req.getSession()).thenReturn(session);
         new RegServlet().doPost(req, resp);
         User user = store.findUserByEmail("test");
         assertThat(user.getName(), is("Andrew"));
