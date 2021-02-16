53aec905e6861bd3f52c38930f22e86403d81c29
reybos <andreybosiy@yandex.ru>
2021-02-16 20:36:39 +0300

    4.1. JSON формат. [#456035]

--- src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
+++ src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
@@ -1,13 +1,23 @@
 package ru.job4j.dream.servlet;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.job4j.dream.store.PsqlStore;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class GreetingServlet extends HttpServlet {
+    private static final Logger LOG = LoggerFactory.getLogger(PsqlStore.class.getName());
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
@@ -16,6 +26,22 @@ public class GreetingServlet extends HttpServlet {
         String name = req.getParameter("name");
         PrintWriter writer = new PrintWriter(resp.getOutputStream());
         writer.println("Nice to meet you, " + name);
+        writer.flush();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        JsonObject data = new Gson().fromJson(req.getReader(), JsonObject.class);
+        String name = data.get("name").getAsString();
+        String answer = "Nice to meet you, " + name;
+        Map<String, String> options = new LinkedHashMap<>();
+        options.put("answer", answer);
+        String json = new Gson().toJson(options);
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        writer.write(json);
         writer.flush();
     }
 }
