68a0c5422881f97ae9bfb35524740454312056d1
reybos <andreybosiy@yandex.ru>
2021-02-18 11:35:31 +0300

    удаление фотографии из файловой системы

--- src/main/java/ru/job4j/dream/servlet/CandidateDeleteServlet.java
+++ src/main/java/ru/job4j/dream/servlet/CandidateDeleteServlet.java
@@ -1,5 +1,7 @@
 package ru.job4j.dream.servlet;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.job4j.dream.model.Candidate;
 import ru.job4j.dream.store.PsqlStore;
 
@@ -7,15 +9,27 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
 
 public class CandidateDeleteServlet extends HttpServlet {
+    private static final Logger LOG = LoggerFactory.getLogger(PsqlStore.class.getName());
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         String id = req.getParameter("id");
         if (id != null) {
             Candidate candidate = PsqlStore.instOf().findCandidateById(Integer.parseInt(id));
+            if (!candidate.getPhotoName().equals("")) {
+                String folderName = "images" + File.separator + "photo";
+                File file = new File(folderName + File.separator + candidate.getPhotoName());
+                if (file.delete()) {
+                    LOG.debug(file + " удален");
+                } else {
+                    LOG.debug(file + " ошибка");
+                }
+            }
             PsqlStore.instOf().delete(candidate);
         }
         resp.sendRedirect(req.getContextPath() + "/candidate.do");
--- src/main/java/ru/job4j/dream/servlet/CandidatePhotoDeleteServlet.java
+++ src/main/java/ru/job4j/dream/servlet/CandidatePhotoDeleteServlet.java
@@ -1,5 +1,7 @@
 package ru.job4j.dream.servlet;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.job4j.dream.model.Photo;
 import ru.job4j.dream.store.PsqlStore;
 
@@ -7,9 +9,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
 
 public class CandidatePhotoDeleteServlet extends HttpServlet {
+    private static final Logger LOG = LoggerFactory.getLogger(PsqlStore.class.getName());
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
@@ -17,6 +22,13 @@ public class CandidatePhotoDeleteServlet extends HttpServlet {
         String candidateId = req.getParameter("candidate_id");
         if (id != null) {
             Photo photo = new Photo(Integer.parseInt(id));
+            String folderName = "images" + File.separator + "photo";
+            File file = new File(folderName + File.separator + photo.getId());
+            if (file.delete()) {
+                LOG.debug(file + " удален");
+            } else {
+                LOG.debug(file + " ошибка");
+            }
             PsqlStore.instOf().delete(photo);
         }
         resp.sendRedirect(req.getContextPath() + "/candidate/edit.do?id=" + candidateId);
